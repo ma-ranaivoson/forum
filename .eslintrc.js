76c65fb9f7b11d0a3bcfe9ae46bbde558893c5a5
ma <rv.nirmalala@gmail.com>
2022-04-12 19:36:47 +0300

    Configure environment

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', '@vue/typescript/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
+};
