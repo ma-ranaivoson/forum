9bbd79450e853d8c3f09ac4c9b7dd77e419910f4
ma <rv.nirmalala@gmail.com>
2022-04-23 12:47:19 +0300

    Add PageNotFound

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-full text-center">
+    <h1>Not found</h1>
+    <router-link :to="{name: 'Home'}">Read some cool threads</router-link>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageNotFound',
+});
+</script>
+
+<style></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import * as VueRouter from "vue-router";
+import sourceData from "@/data.json";
 
 import PageHome from "@/components/PageHome.vue";
 import PageThreadShow from "@/components/PageThreadShow.vue";
+import PageNotFound from "@/components/PageNotFound.vue";
 
 const routes = [
   {
@@ -14,6 +16,28 @@ const routes = [
     component: PageThreadShow,
     name: "ThreadShow",
     props: true,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    beforeEnter(to: any, from: any, next: any) {
+      // Check if resources exists
+      const threadExists = sourceData.threads.find((thread) => thread.id === to.params.id);
+      // Normal redirection if exists
+      if (threadExists) {
+        next();
+        // Redirect to NotFound if not
+      } else {
+        next({
+          name: "NotFound",
+          params: { pathMatch: to.path.substring(1).split("/") },
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+    },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: PageNotFound,
   },
 ];
 
